9e4f4d0ad72e9bc08de55a789fe5dd654c71200d
loveorange <2643500542@qq.com>
2021-07-03 00:08:44 +0800

    fix some big bug

--- js/plugins.js
+++ js/plugins.js
@@ -1,6 +1,6 @@
 /**
  * Single Page Nav Plugin
- * Copyright (c) 2013 Chris wojcik <hello@chriswojcik.net>
+ * Copyright (c) 2013 Chris Wojcik <hello@chriswojcik.net>
  * Dual licensed under MIT and GPL.
  * @author Chris Wojcik
  * @version 1.1.0
@@ -8,25 +8,23 @@
 
 // Utility
 if (typeof Object.create !== 'function') {
-    Object.create = function (obj) {
-        function F() {
-        }
-
+    Object.create = function(obj) {
+        function F() {}
         F.prototype = obj;
         return new F();
     };
 }
 
-(function ($, window, document) {
+(function($, window, document, undefined) {
     "use strict";
-
-    const SinglePageNav = {
-
-        init: function (options, container) {
-
+    
+    var SinglePageNav = {
+        
+        init: function(options, container) {
+            
             this.options = $.extend({}, $.fn.singlePageNav.defaults, options);
-
-            this.container = container;
+            
+            this.container = container;            
             this.$container = $(container);
             this.$links = this.$container.find('a');
 
@@ -36,7 +34,7 @@ if (typeof Object.create !== 'function') {
 
             this.$window = $(window);
             this.$htmlbody = $('html, body');
-
+            
             this.$links.on('click.singlePageNav', $.proxy(this.handleClick, this));
 
             this.didScroll = false;
@@ -44,16 +42,16 @@ if (typeof Object.create !== 'function') {
             this.setTimer();
         },
 
-        handleClick: function (e) {
-            const self = this,
-                link = e.currentTarget,
-                $elem = $(link.hash);
+        handleClick: function(e) {
+            var self  = this,
+                link  = e.currentTarget,
+                $elem = $(link.hash);  
 
-            e.preventDefault();
+            e.preventDefault();             
 
             if ($elem.length) { // Make sure the target elem exists
 
-
+                
                 // Prevent active link from cycling during the scroll
                 self.clearTimer();
 
@@ -63,9 +61,9 @@ if (typeof Object.create !== 'function') {
                 }
 
                 self.setActiveLink(link.hash);
-
-                self.scrollTo($elem, function () {
-
+                
+                self.scrollTo($elem, function() { 
+                 
                     if (self.options.updateHash) {
                         document.location.hash = link.hash;
                     }
@@ -76,21 +74,21 @@ if (typeof Object.create !== 'function') {
                     if (typeof self.options.onComplete === 'function') {
                         self.options.onComplete();
                     }
-                });
-            }
+                });                            
+            }     
         },
-
-        scrollTo: function ($elem, callback) {
-            const self = this;
-            const target = self.getCoords($elem).top;
-            let called = false;
+        
+        scrollTo: function($elem, callback) {
+            var self = this;
+            var target = self.getCoords($elem).top;
+            var called = false;
 
             self.$htmlbody.stop().animate(
-                {scrollTop: target},
-                {
+                {scrollTop: target}, 
+                { 
                     duration: self.options.speed,
-                    easing: self.options.easing,
-                    complete: function () {
+                    easing: self.options.easing, 
+                    complete: function() {
                         if (typeof callback === 'function' && !called) {
                             callback();
                         }
@@ -99,77 +97,77 @@ if (typeof Object.create !== 'function') {
                 }
             );
         },
-
-        setTimer: function () {
-            const self = this;
-
-            self.$window.on('scroll.singlePageNav', function () {
+        
+        setTimer: function() {
+            var self = this;
+            
+            self.$window.on('scroll.singlePageNav', function() {
                 self.didScroll = true;
             });
-
-            self.timer = setInterval(function () {
+            
+            self.timer = setInterval(function() {
                 if (self.didScroll) {
                     self.didScroll = false;
                     self.checkPosition();
                 }
             }, 250);
-        },
-
-        clearTimer: function () {
+        },        
+        
+        clearTimer: function() {
             clearInterval(this.timer);
             this.$window.off('scroll.singlePageNav');
             this.didScroll = false;
         },
-
+        
         // Check the scroll position and set the active section
-        checkPosition: function () {
-            const scrollPos = this.$window();
-            const currentSection = this.getCurrentSection(scrollPos);
+        checkPosition: function() {
+            var scrollPos = this.$window.scrollTop();
+            var currentSection = this.getCurrentSection(scrollPos);
             this.setActiveLink(currentSection);
-        },
-
-        getCoords: function ($elem) {
+        },        
+        
+        getCoords: function($elem) {
             return {
                 top: Math.round($elem.offset().top) - this.options.offset
             };
         },
-
-        setActiveLink: function (href) {
-            const $activeLink = this.$container.find("a[href='" + href + "']");
-
+        
+        setActiveLink: function(href) {
+            var $activeLink = this.$container.find("a[href='" + href + "']");
+                            
             if (!$activeLink.hasClass(this.options.currentClass)) {
                 this.$links.removeClass(this.options.currentClass);
                 $activeLink.addClass(this.options.currentClass);
             }
-        },
-
-        getCurrentSection: function (scrollPos) {
-            let i, hash, coords, section;
-
+        },        
+        
+        getCurrentSection: function(scrollPos) {
+            var i, hash, coords, section;
+            
             for (i = 0; i < this.$links.length; i++) {
                 hash = this.$links[i].hash;
-
+                
                 if ($(hash).length) {
                     coords = this.getCoords($(hash));
-
+                    
                     if (scrollPos >= coords.top - this.options.threshold) {
                         section = hash;
                     }
                 }
             }
-
+            
             // The current section or the first link
             return section || this.$links[0].hash;
         }
     };
-
-    $.fn.singlePageNav = function (options) {
-        return this.each(function () {
-            const singlePageNav = Object.create(SinglePageNav);
+    
+    $.fn.singlePageNav = function(options) {
+        return this.each(function() {
+            var singlePageNav = Object.create(SinglePageNav);
             singlePageNav.init(options, this);
         });
     };
-
+    
     $.fn.singlePageNav.defaults = {
         offset: 0,
         threshold: 120,
@@ -181,112 +179,14 @@ if (typeof Object.create !== 'function') {
         onComplete: false,
         beforeStart: false
     };
-
+    
 })(jQuery, window, document);
 
 
-window.attachEvent = undefined;
-(function (e) {
-    let t = {
-        topSpacing: 0,
-        bottomSpacing: 0,
-        className: "is-sticky",
-        wrapperClassName: "sticky-wrapper",
-        center: false,
-        getWidthFrom: ""
-    }, n = e(window), r = e(document), i = [], s = n.height(), o = function () {
-        const t = n.scrollTop(), o = r.height(), u = o - s, a = t > u ? u - t : 0;
-        for (let f = 0; f < i.length; f++) {
-            const l = i[f], c = l.stickyWrapper.offset().top, h = c - l.topSpacing - a;
-            if (t <= h) {
-                if (l.currentTop !== null) {
-                    l.stickyElement.css("position", "").css("top", "");
-                    l.stickyElement.parent().removeClass(l.className);
-                    l.currentTop = null
-                }
-            } else {
-                let p = o - l.stickyElement.outerHeight() - l.topSpacing - l.bottomSpacing - t - a;
-                if (p < 0) {
-                    p = p + l.topSpacing
-                } else {
-                    p = l.topSpacing
-                }
-                if (l.currentTop !== p) {
-                    l.stickyElement.css("position", "fixed").css("top", p);
-                    if (typeof l.getWidthFrom !== "undefined") {
-                        l.stickyElement.css("width", e(l.getWidthFrom).width())
-                    }
-                    l.stickyElement.parent().addClass(l.className);
-                    l.currentTop = p
-                }
-            }
-        }
-    }, u = function () {
-        s = n.height()
-    }, a = {
-        init: function (n) {
-            const r = e.extend(t, n);
-            return this.each(function () {
-                const t = e(this);
-                const n = t.attr("id");
-                const s = e("<div></div>").attr("id", n + "-sticky-wrapper").addClass(r.wrapperClassName);
-                t.wrapAll(s);
-                if (r.center) {
-                    t.parent().css({width: t.outerWidth(), marginLeft: "auto", marginRight: "auto"})
-                }
-                if (t.css("float") === "right") {
-                    t.css({"float": "none"}).parent().css({"float": "right"})
-                }
-                const o = t.parent();
-                o.css("height", t.outerHeight());
-                i.push({
-                    topSpacing: r.topSpacing,
-                    bottomSpacing: r.bottomSpacing,
-                    stickyElement: t,
-                    currentTop: null,
-                    stickyWrapper: o,
-                    className: r.className,
-                    getWidthFrom: r.getWidthFrom
-                })
-            })
-        }, update: o, unstick: function () {
-            return this.each(function () {
-                const t = e(this);
-                let removeIdx = -1;
-                for (let n = 0; n < i.length; n++) {
-                    if (i[n].stickyElement.get(0) === t.get(0)) {
-                        removeIdx = n
-                    }
-                }
-                if (removeIdx !== -1) {
-                    i.splice(removeIdx, 1);
-                    t.unwrap();
-                    t.removeAttr("style")
-                }
-            })
-        }
-    };
-    if (window.addEventListener) {
-        window.addEventListener("scroll", o, false);
-        window.addEventListener("resize", u, false)
-    } else if (window.attachEvent) {
-        window.attachEvent("onscroll", o);
-        window.attachEvent("onresize", u)
-    }
-    e.fn.sticky = function (t) {
-        if (a[t]) {
-            return a[t].apply(this, Array.prototype.slice.call(arguments, 1))
-        } else if (typeof t === "object" || !t) {
-            return a.init.apply(this, arguments)
-        } else {
-            e.error("Method " + t + " does not exist on jQuery.sticky")
-        }
-    };
 
-    e(function () {
-        setTimeout(o, 0)
-    })
-})(jQuery)
+
+
+(function(e){var t={topSpacing:0,bottomSpacing:0,className:"is-sticky",wrapperClassName:"sticky-wrapper",center:false,getWidthFrom:""},n=e(window),r=e(document),i=[],s=n.height(),o=function(){var t=n.scrollTop(),o=r.height(),u=o-s,a=t>u?u-t:0;for(var f=0;f<i.length;f++){var l=i[f],c=l.stickyWrapper.offset().top,h=c-l.topSpacing-a;if(t<=h){if(l.currentTop!==null){l.stickyElement.css("position","").css("top","");l.stickyElement.parent().removeClass(l.className);l.currentTop=null}}else{var p=o-l.stickyElement.outerHeight()-l.topSpacing-l.bottomSpacing-t-a;if(p<0){p=p+l.topSpacing}else{p=l.topSpacing}if(l.currentTop!=p){l.stickyElement.css("position","fixed").css("top",p);if(typeof l.getWidthFrom!=="undefined"){l.stickyElement.css("width",e(l.getWidthFrom).width())}l.stickyElement.parent().addClass(l.className);l.currentTop=p}}}},u=function(){s=n.height()},a={init:function(n){var r=e.extend(t,n);return this.each(function(){var t=e(this);var n=t.attr("id");var s=e("<div></div>").attr("id",n+"-sticky-wrapper").addClass(r.wrapperClassName);t.wrapAll(s);if(r.center){t.parent().css({width:t.outerWidth(),marginLeft:"auto",marginRight:"auto"})}if(t.css("float")=="right"){t.css({"float":"none"}).parent().css({"float":"right"})}var o=t.parent();o.css("height",t.outerHeight());i.push({topSpacing:r.topSpacing,bottomSpacing:r.bottomSpacing,stickyElement:t,currentTop:null,stickyWrapper:o,className:r.className,getWidthFrom:r.getWidthFrom})})},update:o,unstick:function(t){return this.each(function(){var t=e(this);removeIdx=-1;for(var n=0;n<i.length;n++){if(i[n].stickyElement.get(0)==t.get(0)){removeIdx=n}}if(removeIdx!=-1){i.splice(removeIdx,1);t.unwrap();t.removeAttr("style")}})}};if(window.addEventListener){window.addEventListener("scroll",o,false);window.addEventListener("resize",u,false)}else if(window.attachEvent){window.attachEvent("onscroll",o);window.attachEvent("onresize",u)}e.fn.sticky=function(t){if(a[t]){return a[t].apply(this,Array.prototype.slice.call(arguments,1))}else if(typeof t==="object"||!t){return a.init.apply(this,arguments)}else{e.error("Method "+t+" does not exist on jQuery.sticky")}};e.fn.unstick=function(t){if(a[t]){return a[t].apply(this,Array.prototype.slice.call(arguments,1))}else if(typeof t==="object"||!t){return a.unstick.apply(this,arguments)}else{e.error("Method "+t+" does not exist on jQuery.sticky")}};e(function(){setTimeout(o,0)})})(jQuery)
 
 
 
